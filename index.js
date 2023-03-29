924ab0f244fdd02270f0cf8e50b8aa5c40ae9daf
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-28 17:50:54 -0700

    add the elements

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-function getWeather() {
-    const API_KEY = 'ee329596df76a835cfef822c61b16a6a';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`;
-  
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-      })
-      .catch(error => console.error(error));
-  }
-  
+const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
+
+const API_KEY = "ee329596df76a835cfef822c61b16a6a";
+
+const locationInput = document.querySelector("#location");
+const weatherDisplay = document.querySelector("#weather");
+const tempToggle = document.querySelector("#temp-toggle");
+const savedAreasList = document.querySelector("#saved-areas");
+const saveAreaButton = document.querySelector("#save-area");
+const removeAreaButton = document.querySelector("#remove-area");
