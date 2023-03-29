590d7442253ae159b6d88ad3dc37cb4908162caf
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-28 18:06:57 -0700

    create get weatehr fetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const API_ENDPOINT = "https://api.openweathermap.org/data/2.5/weather";
+const weatherSite = "https://api.openweathermap.org/data/2.5/weather";
 
-const API_KEY = "ee329596df76a835cfef822c61b16a6a";
+const apiKey = "ee329596df76a835cfef822c61b16a6a";
 
 const locationInput = document.querySelector("#location");
 const weatherDisplay = document.querySelector("#weather");
@@ -8,3 +8,18 @@ const tempToggle = document.querySelector("#temp-toggle");
 const savedAreasList = document.querySelector("#saved-areas");
 const saveAreaButton = document.querySelector("#save-area");
 const removeAreaButton = document.querySelector("#remove-area");
+
+
+let currentLocation;
+let currentData;
+let currentUnit = "imperial";
+let savedAreas = [];
+
+async function fetchWeatherData(location){
+    const url = `${weatherSite}?q=${location}&units=${currentUnit}&appid=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+fetchWeatherData();{
+console.log("this is running");}
