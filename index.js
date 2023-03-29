65669c96e5f5c1ab270f963f6730087d3eaee9c9
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-29 07:00:10 -0700

    more code

--- index.js
+++ index.js
@@ -9,17 +9,63 @@ const savedAreasList = document.querySelector("#saved-areas");
 const saveAreaButton = document.querySelector("#save-area");
 const removeAreaButton = document.querySelector("#remove-area");
 
-
-let currentLocation;
+let currentLocation = "";
 let currentData;
 let currentUnit = "imperial";
 let savedAreas = [];
 
-async function fetchWeatherData(location){
-    const url = `${weatherSite}?q=${location}&units=${currentUnit}&appid=${apiKey}`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+async function fetchWeatherData(location) {
+  const url = `${weatherSite}?q=${currentLocation}&units=${currentUnit}&appid=${apiKey}`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data;
 }
-fetchWeatherData();{
-console.log("this is running");}
+fetchWeatherData()
+console.log("show data")
+function processWeatherData(data) {
+  const processedData = {
+    location: data.name,
+    temperature: data.main.temp,
+    description: data.weather[0].description,
+    icon: data.weather[0].icon,
+    time: new Date(data.dt * 1000).toLocaleTimeString(),
+  };
+  return processedData;
+}
+
+function displayWeatherData(data) {
+  const { location, temperature, description, icon, time } = data;
+  const html = `
+    <h2>${location}</h2>
+    <p>${description}</p>
+    <img src="https://openweathermap.org/img/w/${icon}.png" alt="${description}">
+    <p>${temperature}°${currentUnit === "imperial" ? "F" : "C"}</p>
+    <p>Last updated at ${time}</p>
+    `;
+  weatherDisplay.innerHTML = html;
+}
+
+function toggleTemperatureUnits() {
+  currentUnit = currentUnit === "imperial" ? "metric" : "imperial";
+  if (currentData) {
+    displayWeatherData(currentData);
+  }
+}
+
+function saveLocationAsSavedArea() {
+  if (currentLocation && !savedAreas.includes(currentLocation)) {
+    const li = document.createElement("li");
+    li.textContent = currentLocation;
+    savedAreasList.appendChild(li);
+  }
+}
+
+function removeLocationFromSavedAreas() {
+  const index = savedAreas.indexOf(currentLocation);
+  if (index !== -1) {
+    savedAreas.splice(index, 1);
+    const li = savedAreasList.querySelector(`li:nth-of-type(${index + 1})`);
+    savedAreasList.removeChild(li);
+  }
+}
